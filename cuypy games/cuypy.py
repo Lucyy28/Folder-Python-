4e2e18246a6d6a7216b1709c5b13a618ebf6f380
= <=>
2024-10-29 09:41:41 +0700

    5 soal nilai

---
+++ cuypy games/cuypy.py
@@ -0,0 +1,14 @@
+WM = "Welcome To Cuypy Games"
+
+print("\t**************************************")
+print(f"\t******* {WM} *******")
+print("\t**************************************")
+
+
+
+nomor = 4
+
+if nomor == 4:
+    print("Selamat kamu mendapatkan CUYPY tersebut!") 
+else:
+    print("Waduh salah CUYPY ada di nomor lain ")
---
+++ cuypy games/lelele.py
@@ -0,0 +1,11 @@
+nama = "Firdan Ardiansyah"
+usia = 17
+
+print("\t*****************")
+print("\t**** WELCOME ****")
+print("\t*****************")
+
+print (f'''
+    Hai Perkenalkan nama saya {nama}
+    dan usia saya baru {usia}
+''')
